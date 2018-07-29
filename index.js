314e043857936e7ef3bb2c4a2cc2359229e6fef7
khalid jamal <kh.jamal95@gmail.com>
2018-07-29 08:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function BoardMember(name, homeState, training){
   
   this.veto = function(){ return "No, I must disagree"}
   this.approve = function(){ return "You can do that!"}
-  this.doCharity = function(){ return "No, I must disagree"}
+  this.doCharity = function(){ return "I like to help people."}
   this.veto = function(){ return "No, I must disagree"}
 }
 
